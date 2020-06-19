cb9ee194e8130d314e6fdafeec540f93cd45aa82
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-19 21:14:34 +0530

    modal relationship

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,25 @@
 
                   <a href="/posts/create" class="btn btn-primary">Create</a>
                   <h1>Your Blog Posts</h1>
+                  @if( count($posts) >0)
+                    <table class="table table-striped">
+                      <tr>
+                        <th>Title</th>
+                        <th></th>
+                      <th></th>
+                      </tr>
+                      @foreach ($posts as $post)
+
+                        <tr>
+                          <th>{{$post->title}}</th>
+                          <th><a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a></th>
+                          <th></th>
+                        </tr>
+                      @endforeach
+                    </table>
+                  @else
+                    <p>Your have no posts</p>
+                  @endif
                 </div>
             </div>
         </div>
