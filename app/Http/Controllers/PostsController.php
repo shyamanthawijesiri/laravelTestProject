18950c85d913463203f8cb0487e5a34cbcb67a42
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-06-19 21:59:17 +0530

    git auth gaurd

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,14 @@ use DB;
 
 class PostsController extends Controller
 {
+
+    // @return void
+
+     public function __construct()
+     {
+         $this->middleware('auth',['except' => ['index','show']]);
+     }
+
     /**
      * Display a listing of the resource.
      *
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,10 +6,13 @@
  <hr>
  <small>written on {{$post->created_at}}</small>
  <hr>
- <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
- {!!Form::open(['action'=>['PostsController@destroy', $post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
- {{Form::hidden('_method','DELETE')}}
- {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
- {!!Form::close()!!}
-
+   @if(!Auth::guest())
+     @if(Auth:user()->id== $post->user_id)
+       <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+       {!!Form::open(['action'=>['PostsController@destroy', $post->id],'method'=>'POST', 'class'=>'pull-right'])!!}
+       {{Form::hidden('_method','DELETE')}}
+       {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+       {!!Form::close()!!}
+    @endif
+  @endif
 @endsection
